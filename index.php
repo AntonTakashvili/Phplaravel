9d5d2954d6772fdecacaacef8f7b4d8019020478
Anton <44674819+CapitanTeemo@users.noreply.github.com>
2021-10-11 19:22:52 +0400

    update

--- index.php
+++ index.php
@@ -1,33 +1,75 @@
-<?php 
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "users";
+<!doctype html>
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>HM</title>
+	<style >
+		table,th,td{
+			border: 1px solid black;
+		}
+	</style>
+</head>
+<body>
 
-	$conn=mysqli_connect($servername,$username,$password,$dbname);
-	if(!$conn){
-		die("connertion Failed: ". mysqli_connect_error());
-	}
+	<?php 
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "users";
 
-	$sql = "INSERT INTO user(name,surname,birthday,registered_at)
-	VALUES('Ana', 'Qavtaradze', '25/02/1999', 'facebook,instagram'),
-	('Nika', 'Apshinashvili', '1', 'facebook'),
-	('Giorgi', 'Giorgadze', '10', 'facebook')";
-	
-	if ($conn->query($sql) === TRUE) {
-  	echo "New record created successfully";
-	} else {
-  	echo "Error: " . $sql . "<br>" . $conn->error;
-	}
+		$conn=mysqli_connect($servername,$username,$password,$dbname);
+		if(!$conn){
+			die("connertion Failed: ". mysqli_connect_error());
+		}
 
-	
-	$sql="SELECT * FROM `user`";
-	$results=mysqli_query($conn,$sql);
+		$sql = "INSERT INTO user(name,surname,birthday,registered_at)
+		VALUES('Ana', 'Qavtaradze', '1999-02-12', 'facebook,instagram'),
+		('Nika', 'Apshinashvili', '2004-09-21', 'facebook,Twitter'),
+		('Giorgi', 'Giorgadze', '1992-12-10', 'facebook'),
+		('Lasha', 'Bolqvadze','2000-05-05','instagram,Vk,facebook')";
+		
+		if ($conn->query($sql) === TRUE) {
+	  	echo "New record created successfully" . "<br>";
+		} else {
+	  	echo "Error: " . $sql . "<br>" . $conn->error;
+		}
 
-	while ($row=mysqli_fetch_assoc($results)) {
-		echo "id: ". $row["id"]. " Name: ".$row["name"]. " Surname: " 
-		. $row["surname"]. " Birthday: ". $row["birthday"]. " Registreted: " .$row["registered_at"]. "<br>";
-	}
+		
+		$sql="SELECT * FROM `user`";
+		$results=mysqli_query($conn,$sql);
 
-	mysqli_close($conn);
-?>
+		if ($results->num_rows > 0) {
+    		echo "<table><tr><th>ID</th><th>Name</th><th>Surname</th><th>Birthday</th><th>Registered</th></tr>";
+			while ($row=mysqli_fetch_assoc($results)) {
+				/*echo "id: ". $row["id"]. " Name: ".$row["name"]. " Surname: " 
+				. $row["surname"]. " Birthday: ". $row["birthday"]. " Registreted: " .$row["registered_at"]. "<br>";*/
+				echo "<tr><td> ". $row["id"]. "</td><td> ".$row["name"]. "</td><td> " 
+				. $row["surname"]. " </td><td> ". $row["birthday"]. "</td><td> " .$row["registered_at"]. "</td></tr>";
+			}
+			echo "</table>";
+		}else{
+			echo "0 results";
+		}
+
+		$sql = "UPDATE user SET  surname = 'Shengelia' WHERE id = 58";
+		if (mysqli_query($conn, $sql)) {
+		  echo "Record updated successfully";
+		} else {
+		  echo "Error updating record: " . mysqli_error($conn);
+		}
+
+		$sql = "DELETE FROM user WHERE id=57";
+
+		if ($conn->query($sql) === TRUE) {
+		  echo "Record deleted successfully";
+		} else {
+		  echo "Error deleting record: " . $conn->error;
+		}
+
+		mysqli_close($conn);
+	?>
+
+
+</body>
+</html>
--- user.sql
+++ user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 09, 2021 at 07:09 PM
+-- Generation Time: Oct 11, 2021 at 05:24 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -31,34 +31,9 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `surname` varchar(100) NOT NULL,
-  `birthday` int(11) NOT NULL,
+  `birthday` date NOT NULL,
   `registered_at` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `surname`, `birthday`, `registered_at`) VALUES
-(5, 'Ana', 'Qavtaradze', 25, 'facebook,instagram'),
-(6, 'Ana', 'Qavtaradze', 25, 'facebook,instagram'),
-(7, 'Nika', 'Apshinashvili', 1, 'facebook'),
-(8, 'Giorgi', 'Giorgadze', 10, 'facebook'),
-(9, 'Ana', 'Qavtaradze', 25, 'facebook,instagram'),
-(10, 'Nika', 'Apshinashvili', 1, 'facebook'),
-(11, 'Giorgi', 'Giorgadze', 10, 'facebook'),
-(12, 'Ana', 'Qavtaradze', 25, 'facebook,instagram'),
-(13, 'Nika', 'Apshinashvili', 1, 'facebook'),
-(14, 'Giorgi', 'Giorgadze', 10, 'facebook'),
-(15, 'Ana', 'Qavtaradze', 25, 'facebook,instagram'),
-(16, 'Nika', 'Apshinashvili', 1, 'facebook'),
-(17, 'Giorgi', 'Giorgadze', 10, 'facebook'),
-(18, 'Ana', 'Qavtaradze', 25, 'facebook,instagram'),
-(19, 'Nika', 'Apshinashvili', 1, 'facebook'),
-(20, 'Giorgi', 'Giorgadze', 10, 'facebook'),
-(21, 'Ana', 'Qavtaradze', 25, 'facebook,instagram'),
-(22, 'Nika', 'Apshinashvili', 1, 'facebook'),
-(23, 'Giorgi', 'Giorgadze', 10, 'facebook');
 
 --
 -- Indexes for dumped tables
@@ -78,7 +53,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
